78b9707e1bf255588f40aae95d1f5c6fe0e9ff64
Sachin shrimangale <160982493+sachinshrimangale@users.noreply.github.com>
2024-08-11 01:09:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+
+
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[toCurr.value.toLowerCase()];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+
+
+// below code is for mode feature!
+let modebtn = document.querySelector("#modebtn");
+let crntmode ="light";
+modebtn.addEventListener("click",()=>{
+    if(crntmode === "light")
+    {
+        crntmode ="dark"
+        document.body.style.backgroundColor = "black";
+    }
+    else{
+        crntmode ="light"
+       document.body.style.backgroundColor ="white";
+       document.body.style.border =" black";
+    }
+    console.log(crntmode)
+})
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+  });
+  
+  window.addEventListener("load", () => {
+    updateExchangeRate();
+  });
+  
+//  404 =>Server Error!!
